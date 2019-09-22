ad2f0599b53584109b339766fc235fef77eadaf2
Jewel591 <Zhc456654456>
2019-09-22 21:58:58 +0800

    develop version 0.1

--- amazeScan.py
+++ amazeScan.py
@@ -1,1 +1,33 @@
 # start py
+import subprocess
+import time
+# 不使用 script-trace 命令输出太繁琐，使用 -vv 合适
+#### ftp
+# ftp-anon.nse
+# ftp-brute.nse
+# ftp-vuln-cve2010-4221.nse
+# ftp-vsftpd-backdoor.nse
+# ftp-proftpd-backdoor.nse
+### ssh
+# ssh-brute.nse
+# ssh-publickey-acceptance.nse 尝试已知公钥登录
+### mysql
+# mysql-enum.nse mysql 5.x 存在漏洞，用户名枚举
+# mysql-brute.nse
+# mysql-vuln-cve2012-2122.nse 绕过认证
+
+time_start = time.time()
+command_ftpcheck = "nmap -Pn --open www.selenachina.cn --script='" \
+                   "ftp-anon.nse," \
+                   "ftp-brute.nse," \
+                   "ftp-vuln-cve2010-4221.nse," \
+                   "ftp-vsftpd-backdoor.nse," \
+                   "ftp-proftpd-backdoor.nse," \
+                   "ssh-brute.nse," \
+                   "ssh-publickey-acceptance.nse," \
+                   "mysql-enum.nse,mysql-brute.nse," \
+                   "mysql-vuln-cve2012-2122.nse," \
+                   "mysql-brute.nse' -vv"
+subprocess.call(command_ftpcheck,shell=True)
+time_end = time.time()
+print("扫描耗时："+time_end-time_start+"s")
--- issues.md
+++ issues.md
@@ -1,1 +1,8 @@
-##1.1 如何获取 nmap 脚本的检测结果
+## 1.1 如何获取 nmap 脚本的检测结果
+## 1.2 Python 如何执行 shell 命令？
+- os.system() 比较老的模块，不建议使用
+- subprocess 推荐使用
+    - subpeocess.call(command,shell=True) 会直接打印出结果。
+    - subprocess.Popen(command, shell=True) 也可以是subprocess.Popen(command, stdout=subprocess.PIPE, shell=True) 这样就可以输出结果了。
+
+> 如果command不是一个可执行文件，shell=True是不可省略的，shell=True意思是shell下执行command
